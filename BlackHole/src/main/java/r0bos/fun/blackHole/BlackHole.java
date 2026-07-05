f8085650b683f546a9b3b7c84e13e42b19427b35
Tanay-Gali <tanaygali5@gmail.com>
2026-07-05 16:43:16 -0400

    Add files via upload

---
+++ BlackHole/src/main/java/r0bos/fun/blackHole/BlackHole.java
@@ -0,0 +1,24 @@
+package r0bos.fun.blackHole;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class BlackHole extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this,this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+    
+}
---
+++ BlackHole/src/main/java/r0bos/fun/blackHole/BlackHoleCommand.java
@@ -0,0 +1,20 @@
+package r0bos.fun.blackHole;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class BlackHoleCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+
+
+
+
+
+
+
+        return false;
+    }
+}
